b0ec3c5a6736ffac98ace4903a76ba34d46da039
LucasHfernandez <36281566+LucasHfernandez@users.noreply.github.com>
2019-04-10 08:11:41 -0300

    Add files via upload

---
+++ Entidades/Calculadora.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Calculadora
+    {
+        public double Operar(Numero num1, Numero num2, string operador)
+        {
+
+        }
+
+        private static string ValidarOperador(string operador)
+        {
+
+            string retorno;
+
+            if(operador == "+" || operador == "-" || operador == "*" || operador == "/")
+            {
+                retorno = operador;
+
+            }
+            else
+            {
+                retorno = "+";
+            }
+
+            return retorno;
+        }
+    }
+}
---
+++ Entidades/Numero.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Numero
+    {
+        private double numero;
+
+        public Numero()
+        {
+            this.numero = 0;
+        }
+
+        public Numero(double numero)
+        {
+            this.numero = numero;
+        }
+
+        private double ValidarNumero(string strNumero)
+        {
+            int i;
+            int flag = 1;
+            bool valor;
+            double retorno = 0;
+
+
+            for(i = 0; i < strNumero.Length; i++)
+            {
+                if (strNumero[i] < '0' || strNumero[i] > '9')
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+
+            if(flag == 1)
+            {
+                valor = double.TryParse(strNumero, out retorno);
+            }
+
+            return retorno;
+        }
+
+        public double DecimalBinario(double numero)
+        {
+            string mensaje;
+
+            if(numero > 0)
+            {
+                
+            }
+            else
+            {
+                mensaje = "Valor invalido";
+            }
+        }
+    }
+}
