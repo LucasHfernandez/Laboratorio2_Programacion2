9f4ada044c11e04340bff502e5039f5aa86e2eda
LucasHfernandez <fernandezlucasivan@gmail.com>
2019-05-02 22:05:22 -0300

    Ejercicio parcial 2 sin terminar

--- Fernandez.Lucas.2C/Entidades/Moto.cs
+++ Fernandez.Lucas.2C/Entidades/Moto.cs
@@ -8,9 +8,44 @@ namespace Entidades
 {
     public class Moto : Vehiculo
     {
+        private int cilindrada;
+        private static short ruedas;
+        private static int valorHora;
+
+        public string ConsultarDatos()
+        {
+            StringBuilder datos = new StringBuilder();
+            datos.AppendLine("**Datos de la Moto**");
+            datos.AppendFormat("\n\nPatente: {0}", Patente);
+            datos.AppendFormat("\nPatente: {0}", Patente);
+        }
+
         public bool Equals(object obj)
         {
-            
+            return obj is Moto;
         }
+
+        static Moto()
+        {
+            valorHora = 30;
+            ruedas = 2;
+        }
+
+        public Moto(string patente, int cilindrada) : base(patente)
+        {
+            this.cilindrada = cilindrada;
+        }
+
+        public Moto(string patente, int cilindrada, short ruedas) : this(patente, cilindrada)
+        {
+            Moto.ruedas = ruedas;
+        }
+
+        public Moto(string patente, int cilindrada, short ruedas, int valorHora) : this(patente, cilindrada, ruedas)
+        {
+            Moto.valorHora = valorHora;
+        }
+
+
     }
 }
