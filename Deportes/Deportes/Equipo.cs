920cdae3a4e12d868caafecda59aa32734c0afbf
LucasHfernandez <fernandezlucasivan@gmail.com>
2019-05-22 20:07:11 -0300

    ejercicio 47 no terminado

---
+++ Deportes/Deportes/Equipo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deportes
+{
+    public abstract class Equipo
+    {
+        public string nombre;
+        public DateTime fechaCreacion;
+
+        public static bool operator ==(Equipo equipoUno, Equipo equipoDos)
+        {
+            bool retorno = false;
+
+            if(equipoUno.nombre == equipoDos.nombre && equipoUno.fechaCreacion == equipoDos.fechaCreacion)
+            {
+                retorno = true;
+            }
+
+            return retorno;
+        }
+
+        public static bool operator !=(Equipo equipoUno, Equipo equipoDos)
+        {
+            return !(equipoUno == equipoDos);
+        }
+
+
+        public string Ficha()
+        {
+            StringBuilder equipo = new StringBuilder();
+
+            equipo.AppendFormat("[{0}] fundado el [{1}]", nombre, fechaCreacion.ToString());
+
+            return equipo.ToString();
+        }
+    }
+}
---
+++ Deportes/Deportes/Torneo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deportes
+{
+    public class Torneo<T> where T : Equipo
+    {
+        public List<string> equipos;
+        public string nombre;
+
+        public static bool operator ==(Torneo<T> equipo, T torneo)
+        {
+            bool retorno = false;
+
+            foreach(T equip in equipo.equipos)
+            {
+
+            }
+        }
+    }
+}
